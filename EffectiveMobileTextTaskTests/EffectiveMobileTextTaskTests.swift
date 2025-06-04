e81c2fe41f51c066ea541f13a76a16a9fd80d346
Stas Telnov <telnov-si@m13.ru>
2025-06-04 09:34:22 +0300

    Initial commin

---
+++ EffectiveMobileTextTaskTests/EffectiveMobileTextTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTextTaskTests.swift
+//  EffectiveMobileTextTaskTests
+//
+//  Created by Stas Telnov on 04.06.2025.
+//
+
+import Testing
+@testable import EffectiveMobileTextTask
+
+struct EffectiveMobileTextTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
